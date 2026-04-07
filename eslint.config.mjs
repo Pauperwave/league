ce40cbed7f692d9db5f64c1d12c1b996c58c8b06
Emanuele Nardi <emanuelenardi.dev@gmail.com>
2026-04-07 08:19:05 +0200

    feat: add @stylistic/eslint-plugin for enhanced linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default withNuxt(
+  {
+    plugins: {
+      '@stylistic': stylistic
+    }
+  },
   {
     files: ['**/*.ts', '**/*.vue'],
     rules: {
