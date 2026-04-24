4a7a83bc26d41e47bb54538f35fbffd863141cbe
Emanuele Nardi <emanuelenardi.dev@gmail.com>
2026-04-24 02:16:16 +0200

    feat(ui): replace img with NuxtImg component for AppLogo

--- app/components/Layout/AppLogo.vue
+++ app/components/Layout/AppLogo.vue
@@ -1,9 +1,9 @@
 <!-- app\components\Layout\AppLogo.vue -->
 <template>
   <!-- In dark mode use the original logo, in light mode use the inverted version -->
-  <img
-    src="~/assets/logo/pauperwave.png"
+  <NuxtImg
+    src="/logo/pauperwave.png"
     alt="Pauperwave"
     class="h-6 w-auto dark:invert-0 invert"
-  >
+  />
 </template>
